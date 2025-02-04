55fa51575b17904cddba2d1c6e406abb4300efe2
Justin S Qiu <jsq@nlpgpu-login.seas.upenn.edu>
2025-02-04 03:17:21 -0500

    script working

--- test_script.sh
+++ test_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#
-#SBATCH --job-name=test
-#SBATCH --output=res.txt
-#
-#SBATCH --ntasks=1
-#SBATCH --time=60:00
-#SBATCH --mem-per-cpu=200
+#SBATCH --job-name=test_job
+#SBATCH --output=test_output.txt
+#SBATCH --error=test_error.txt
+#SBATCH --partition=p_nlp
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1GB
+#SBATCH --time=00:05:00
 
-srun hostname
-srun sleep 60
+echo "Hello World"
